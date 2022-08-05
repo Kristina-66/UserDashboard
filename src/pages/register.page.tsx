c4f9f4d65923c25010be1ad9aad330da07d0184b
Kristina <dead.girl.2012@mail.ru>
2022-08-05 18:51:57 +0400

    add style

--- src/pages/register.page.tsx
+++ src/pages/register.page.tsx
@@ -14,8 +14,8 @@ import { styled } from "@mui/material/styles";
 
 const LoadingButton = styled(_LoadingButton)`
   padding: 0.6rem 0;
-  background-color: #f9d13e;
-  color: #2363eb;
+  background-color: #rgb(136 190 207);
+  color: dark;
   font-weight: 500;
   &:hover {
     background-color: #ebc22c;
